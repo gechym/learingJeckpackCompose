72dc98ce243bff24ff568e25f6c557bd035838ee
gechym <nguyenducbao166@gmai.com>
2022-04-10 22:54:49 +0700

    add

--- app/src/main/java/com/example/jeckpackcompose/RecipelistFragment.kt
+++ app/src/main/java/com/example/jeckpackcompose/RecipelistFragment.kt
@@ -4,7 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 
 class RecipelistFragment : Fragment() {
@@ -14,9 +24,17 @@ class RecipelistFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        val view = inflater.inflate(R.layout.fragment_recipelist, container, false)
-
-        return view
+        return ComposeView(requireContext()).apply { 
+            setContent { 
+                Column() {
+                    Text(text = "Hello", fontSize = 20.sp)
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = {findNavController().navigate(R.id.action_recipelistFragment_to_recipeFragment) }) {
+                        Text(text = "ClickMe")
+                    }
+                }
+            }
+        }
     }
 
 
