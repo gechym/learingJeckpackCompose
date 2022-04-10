06ad168207b7bd455aa81ccb7e8691c9c4c8f6e0
gechym <nguyenducbao166@gmai.com>
2022-04-10 21:21:30 +0700

    add

---
+++ app/src/main/java/com/example/jeckpackcompose/RecipeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.jeckpackcompose
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+
+
+class RecipeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(text = "Hello các bác")
+                }
+            }
+        }
+    }
+
+
+}
